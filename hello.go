fcce0853a2074ca65a1acd0f064dcdd721b83d19
David <davidwusu128@icloud.com>
2024-09-19 02:47:09 +0100

    Hello with Goroutines

--- hello.go
+++ hello.go
@@ -1,9 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	for i := 0; i < 20; i++ {
-		fmt.Println("Hello World")
+	for i := 0; i < 5; i++ {
+		go func(i int) {
+			fmt.Printf("Hello from goroutine %d\n", i)
+		}(i)
 	}
+	time.Sleep(1 * time.Second)
 }
